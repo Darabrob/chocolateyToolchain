926d9b6fad9e86c30f4e2375271d1969993d61c8
Sebastian Stolz <sebastian.stolz@fbs-fd.de>
2025-02-21 14:10:43 +0100

    Everything hinzugefügt

--- install_toolchain.ps1
+++ install_toolchain.ps1
@@ -9,6 +9,17 @@ if ($exitCode -eq 0) {
     Write-Output "Installation failed with exit code $exitCode."
 }
 
+choco install everything -y
+$exitCode = $LASTEXITCODE
+if ($exitCode -eq 0) {
+    Write-Output "Installation successful."
+} elseif ($exitCode -eq 3010) {
+    Write-Output "Installation successful, but a restart is required."
+} else {
+    Write-Output "Installation failed with exit code $exitCode."
+}
+
+
 
 
 
